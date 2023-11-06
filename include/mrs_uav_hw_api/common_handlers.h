c4053b3d30ef404b6cc830f74b69bd80c0c986bb
Tomas Baca <klaxalk@gmail.com>
2023-11-06 14:11:07 +0100

    wip docs

--- include/mrs_uav_hw_api/common_handlers.h
+++ include/mrs_uav_hw_api/common_handlers.h
@@ -13,16 +13,22 @@ typedef std::function<std::string(void)> getUavName_t;
 typedef std::function<std::string(void)> getBodyFrameName_t;
 typedef std::function<std::string(void)> getWorldFrameName_t;
 
+/**
+ * @brief A tructure with methods and variables provided to the HW API Plugin.
+ */
 struct CommonHandlers_t
 {
+  /**
+   * @brief A 
+   */
   Publishers_t                          publishers;
+
   std::shared_ptr<mrs_lib::Transformer> transformer;
 
   getUavName_t        getUavName;
   getBodyFrameName_t  getBodyFrameName;
   getWorldFrameName_t getWorldFrameName;
 };
-
 
 }  // namespace mrs_uav_hw_api
 
--- include/mrs_uav_hw_api/publishers.h
+++ include/mrs_uav_hw_api/publishers.h
@@ -31,7 +31,6 @@ namespace mrs_uav_hw_api
 typedef std::function<void(const sensor_msgs::NavSatFix &msg)>     publishGNSS_t;
 typedef std::function<void(const sensor_msgs::NavSatStatus &msg)>  publishGNSSStatus_t;
 typedef std::function<void(const mrs_msgs::RtkGps &msg)>           publishRTK_t;
-typedef std::function<void(const sensor_msgs::Range &msg)>         publishDistanceSensor_t;
 typedef std::function<void(const mrs_msgs::HwApiAltitude &msg)>    publishAltitude_t;
 typedef std::function<void(const mrs_msgs::Float64Stamped &msg)>   publishMagnetometerHeading_t;
 typedef std::function<void(const sensor_msgs::MagneticField &msg)> publishMagneticField_t;
@@ -40,12 +39,27 @@ typedef std::function<void(const mrs_msgs::HwApiRcChannels &msg)>  publishRcChan
 typedef std::function<void(const sensor_msgs::BatteryState &msg)>  publishBatteryState_t;
 typedef std::function<void(const sensor_msgs::Imu &msg)>           publishIMU_t;
 
+/**
+ * @brief The distance sensor is expected to be pointing down towards the ground
+ */
+typedef std::function<void(const sensor_msgs::Range &msg)> publishDistanceSensor_t;
+
+/**
+ * @brief The UAV Velocity expressed in the UAV Body frame.
+ */
+typedef std::function<void(const geometry_msgs::Vector3Stamped &msg)>    publishVelocity_t;
+
 typedef std::function<void(const geometry_msgs::PointStamped &msg)>      publishPosition_t;
 typedef std::function<void(const geometry_msgs::QuaternionStamped &msg)> publishOrientation_t;
-typedef std::function<void(const geometry_msgs::Vector3Stamped &msg)>    publishVelocity_t;
 typedef std::function<void(const geometry_msgs::Vector3Stamped &msg)>    publishAngularVelocity_t;
 typedef std::function<void(const nav_msgs::Odometry &msg)>               publishOdometry_t;
 
+/**
+ * @brief A structure with publisher methods provided to the HW API Plugin.
+ * The plugin can call these methods at will to provided data to the MRS UAV System.
+ * In case the HW API Plugin intents to publish data, the respective varible "produces_***"
+ * in the HW API Capabilities of the plugin is supposed to be fill in.
+ */
 struct Publishers_t
 {
   publishIMU_t                 publishIMU;
