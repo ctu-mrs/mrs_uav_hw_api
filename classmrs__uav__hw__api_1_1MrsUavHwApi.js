e9e5e8d624db826816c731ea2cdfbf7fefbb61db
klaxalk <klaxalk@users.noreply.github.com>
2023-12-20 08:14:23 +0000

    deploy: 8ddfa1e0af627e24a8b188f613116a85c9d54efb

---
+++ classmrs__uav__hw__api_1_1MrsUavHwApi.js
@@ -0,0 +1,19 @@
+var classmrs__uav__hw__api_1_1MrsUavHwApi =
+[
+    [ "~MrsUavHwApi", "classmrs__uav__hw__api_1_1MrsUavHwApi.html#ac56c138bfac82f134d2c67558d49e51d", null ],
+    [ "callbackAccelerationHdgCmd", "classmrs__uav__hw__api_1_1MrsUavHwApi.html#af88b2aa16a8e88662cf6c16747acdf82", null ],
+    [ "callbackAccelerationHdgRateCmd", "classmrs__uav__hw__api_1_1MrsUavHwApi.html#a5903740fdd654da8e0b1f0fcd34d852b", null ],
+    [ "callbackActuatorCmd", "classmrs__uav__hw__api_1_1MrsUavHwApi.html#a397145b920c1600b974fa3a5d107411a", null ],
+    [ "callbackArming", "classmrs__uav__hw__api_1_1MrsUavHwApi.html#ad549fd61bfa9996b1aaa49946d814156", null ],
+    [ "callbackAttitudeCmd", "classmrs__uav__hw__api_1_1MrsUavHwApi.html#a537f4a53b84aee069bc3a8dd6a6620f7", null ],
+    [ "callbackAttitudeRateCmd", "classmrs__uav__hw__api_1_1MrsUavHwApi.html#afb549fb51dbf0c05b082b1de9cd6c30b", null ],
+    [ "callbackControlGroupCmd", "classmrs__uav__hw__api_1_1MrsUavHwApi.html#ada32d79a7512a6cba1b38d48a7926f18", null ],
+    [ "callbackOffboard", "classmrs__uav__hw__api_1_1MrsUavHwApi.html#aa4e0e43507391daf90f115d941b5aaee", null ],
+    [ "callbackPositionCmd", "classmrs__uav__hw__api_1_1MrsUavHwApi.html#abfaa7f3e38b567f9005152e8bacb23fb", null ],
+    [ "callbackTrackerCmd", "classmrs__uav__hw__api_1_1MrsUavHwApi.html#a610c4698eb909da8a5ab05b730347318", null ],
+    [ "callbackVelocityHdgCmd", "classmrs__uav__hw__api_1_1MrsUavHwApi.html#a624d2ec97c6c02118e214216614572b7", null ],
+    [ "callbackVelocityHdgRateCmd", "classmrs__uav__hw__api_1_1MrsUavHwApi.html#a47d1818995e6bc815f4e8cad789c4232", null ],
+    [ "getCapabilities", "classmrs__uav__hw__api_1_1MrsUavHwApi.html#a654f724908c2a601f69b66f44f94092f", null ],
+    [ "getStatus", "classmrs__uav__hw__api_1_1MrsUavHwApi.html#a7d8267f792fffb3a6118df0db21d1202", null ],
+    [ "initialize", "classmrs__uav__hw__api_1_1MrsUavHwApi.html#a8889e80844000735944372644daf4a19", null ]
+];
---
+++ search/all_2.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['getbodyframename_14',['getBodyFrameName',['../structmrs__uav__hw__api_1_1CommonHandlers__t.html#abd112a8b4d35872df1bf2d110875cf82',1,'mrs_uav_hw_api::CommonHandlers_t']]],
+  ['getcapabilities_15',['getCapabilities',['../classmrs__uav__hw__api_1_1MrsUavHwApi.html#a654f724908c2a601f69b66f44f94092f',1,'mrs_uav_hw_api::MrsUavHwApi::getCapabilities()'],['../classmrs__uav__hw__api_1_1DummyApi.html#a3328034dca690d917ce2689c1eb13b52',1,'mrs_uav_hw_api::DummyApi::getCapabilities()']]],
+  ['getstatus_16',['getStatus',['../classmrs__uav__hw__api_1_1MrsUavHwApi.html#a7d8267f792fffb3a6118df0db21d1202',1,'mrs_uav_hw_api::MrsUavHwApi::getStatus()'],['../classmrs__uav__hw__api_1_1DummyApi.html#af7bbab47dba560abd5a76a273c07e157',1,'mrs_uav_hw_api::DummyApi::getStatus()']]],
+  ['getuavname_17',['getUavName',['../structmrs__uav__hw__api_1_1CommonHandlers__t.html#add8f6daa450737766d3006bab075cc18',1,'mrs_uav_hw_api::CommonHandlers_t']]],
+  ['getworldframename_18',['getWorldFrameName',['../structmrs__uav__hw__api_1_1CommonHandlers__t.html#a460f462de5f5b703562cc7d4c4bf62e1',1,'mrs_uav_hw_api::CommonHandlers_t']]]
+];
