f7da75d07cd0ffd4ac55019e66369b543f02dc7b
Tomas Baca <Tomas Baca>
2025-09-20 12:16:53 +0200

    updated test scripts

--- test/run_specific_test.sh
+++ test/run_specific_test.sh
@@ -9,6 +9,8 @@ while [ ! -e "build/COLCON_IGNORE" ]; do
   fi
 done
 
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+
 colcon test-result --delete-yes
 
 colcon test --packages-select mrs_uav_hw_api --ctest-args -R 'load_dummy' --event-handlers console_direct+
